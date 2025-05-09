4da4b7929b381401e3d24338ea0433f13105830f
KerkkaiwanMam <kerkkaiwan.ks@gmail.com>
2025-05-09 11:45:53 +0700

    bg

--- finder-frontend/pages/index.vue
+++ finder-frontend/pages/index.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="min-h-screen bg-gradient-to-br from-background via-primary to-secondary-2 text-white flex items-center justify-center">
-    <div class="text-center px-6 py-8 max-w-1/2 bg-white/80 rounded-lg shadow-lg p-8 backdrop-blur-sm animate-fade-in-up">
-      <NuxtImg :src="Images.logo" alt="Logo" class="w-24 h-24 mx-auto mb-4 animate-fade-in-up delay-100" />
+  <div class="min-h-screen bg-gradient-to-br from-secondary-2 via-primary to-black/20 text-white flex items-center justify-center">
+    <div class="text-center px-6 py-8 max-w-1/2 bg-secondary-2/50 rounded-lg shadow-lg p-8 backdrop-blur-sm animate-fade-in-up">
+      <NuxtImg :src="Images.logo" alt="Logo" class="w-max h-auto mx-auto mb-4 mt-8 animate-fade-in-up delay-100" />
       <div
         class="mb-4 inline-block px-3 py-1 rounded-full bg-background/30 text-secondary-2 text-sm font-medium border border-secondary-2-500 animate-fade-in delay-100">
         🚀 Web Programming
       </div>
 
-      <h1 class="text-4xl sm:text-5xl font-extrabold leading-tight mb-4 text-secondary-2 animate-fade-in-up delay-200">
-        Build real world projects<br />
-        to learn and showcase
+      <h1 class="text-2xl sm:text-4xl font-extrabold leading-tight mb-4 text-white/80 animate-fade-in-up delay-200">
+        Young Webmaster Camp ครั้งที่ 20
       </h1>
 
       <p class="text-primary mb-6 text-lg animate-fade-in-up delay-300 px-16">
